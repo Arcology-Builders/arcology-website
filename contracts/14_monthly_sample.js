c16579e25e32376f9fcfe3e08dd4faa31df6ab80
Paul Pham <paul@invisible.college>
2018-08-14 19:48:27 -0400

    Add a public sample monthly contract.

---
+++ contracts/14_monthly_sample.js
@@ -0,0 +1,27 @@
+var params = {
+  type: "monthly",
+  sublesseeName: "Irving Q. Mojo ",
+  sublesseeEmail: "irving@mojo.com",
+  maxSubleaseMonths: 6,
+  moveInDate: "17 September 2018",
+  finalMoveOutDate: "31 March 2018",
+  gracePeriod: "3",
+  bedList: [
+    {
+      bedNumber: "4P",
+      airbnbUrl: "https://www.airbnb.com/rooms/24063724",
+      moveInDate: "17 September 2018",
+      moveOutDate: "on the 17th of a future month, 17 March 2019 at the latest",
+    }
+  ],
+  projectList: [
+  ],
+  monthlyAmount: "1422",
+  monthlyAmountNumber: 1422,
+  lastMonthAmount: "1222 USD",
+  lastMonthAmountNumber: 1222,
+  monthlyCleaningAmount: "50 USD",
+  monthlyCleaningAmountNumber: 50,
+  depositAmount: "1111 USD",
+  depositAmountNumber: 1111,
+};

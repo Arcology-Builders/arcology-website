5d29da677ee362109cc4fa4d920ef42fa0b96b1c
Paul Pham <paul@invisible.college>
2018-08-15 03:53:10 -0400

    Change id's to class's to allow multiple substitutions.

--- contracts/14_monthly_sample.js
+++ contracts/14_monthly_sample.js
@@ -5,7 +5,9 @@ var params = {
   maxSubleaseMonths: 6,
   moveInDate: "17 September 2018",
   finalMoveOutDate: "31 March 2018",
-  gracePeriod: "3",
+  noticeDays: 30,
+  gracePeriod: 3,
+  furniture: "a stack of mattresses with a pea hidden in them",
   bedList: [
     {
       bedNumber: "4P",
@@ -16,12 +18,16 @@ var params = {
   ],
   projectList: [
   ],
-  monthlyAmount: "1422",
-  monthlyAmountNumber: 1422,
-  lastMonthAmount: "1222 USD",
-  lastMonthAmountNumber: 1222,
+  monthlyTotalAmount: "1161 USD",
+  monthlyTotalAmountNumber: 1161,
+  monthlyRentAmount: "1111",
+  monthlyRentAmountNumber: 1111,
+  lastMonthAmount: "1111 USD",
+  lastMonthAmountNumber: 1111,
   monthlyCleaningAmount: "50 USD",
   monthlyCleaningAmountNumber: 50,
   depositAmount: "1111 USD",
   depositAmountNumber: 1111,
+  moveInAmount: "3383 USD",
+  moveInAmountNumber: 3383,
 };

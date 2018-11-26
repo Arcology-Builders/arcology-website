bd3c0209a78046ffe97426c7de16745f3cbe636d
Paul Pham <paul@invisible.college>
2018-11-26 02:55:39 +0000

    Added nested combineCurrencyMaps, good for both sums and snapshots.

--- ssb/ledger-sum.js
+++ ssb/ledger-sum.js
@@ -1,9 +1,9 @@
 const { Map } = require('immutable')
 
 const PRECISION_MAP = new Map({
-	'USD': 2,
-	'ETH': 18,
-	'DAI': 18,
+  'USD': 2,
+  'ETH': 18,
+  'DAI': 18,
 })
 
 
@@ -19,24 +19,36 @@ function createSumObj(params) {
   }
 }
 
+function combineCurrencyMaps( currencyMaps, precisionMap = PRECISION_MAP ) {
+  return currencyMaps.reduce((sum, val, key) => {
+    return val.reduce((sum1, val1, key1) => {
+      let txCurrency = key1,
+        txAmount = Number(val1),
+	precisionPow = Math.pow(10, precisionMap.get(txCurrency)),
+        prevAmount = Math.max(sum1.get(txCurrency, 0), 0),
+	newSum = Math.round((prevAmount + txAmount)*precisionPow)/precisionPow;
+      console.log(`Key: ${txCurrency} Val ${txAmount}`);
+      return sum1.set(txCurrency, newSum)
+    }, sum)
+  }
+  )
+}
+
 // txs is a List of JSON objects, which represent
 // the msg.content.value of a SSB message.
-function computeSum(txs, precisionMap = PRECISION_MAP) {
-  sumsMap = txs.reduce((sum, val, key) => {
-    let txCurrency = val['amountCurrency'],
-        txAmount = Number(val['amountNumber']),
-	precisionPow = Math.pow(10, precisionMap.get(txCurrency)),
-        prevAmount = Math.max(sum.get(txCurrency, 0), 0),
-	newSum = Math.round((prevAmount + txAmount)*precisionPow)/precisionPow;
-    return sum.set(txCurrency, newSum)
-  }, new Map({}),
-  )
+function computeSum(txs) {
+  let currencyMaps = txs.map((x) => {
+    let key = x['amountCurrency'],
+        val = x['amountNumber'];
+    return new Map().set(key, val)}) 
+  console.log(JSON.stringify(currencyMaps))
+  sumsMap = combineCurrencyMaps(currencyMaps)
   startDate = txs.reduce((sum, val, key) => {
-	  return Math.min(val['txTime'], sum)
+    return Math.min(val['txTime'], sum)
   }, Number.MAX_SAFE_INTEGER,
   )
   endDate = txs.reduce((sum, val, key) => {
-	  return Math.max(val['txTime'], sum)
+    return Math.max(val['txTime'], sum)
   }, 0,
   )
 
@@ -59,4 +71,5 @@ function computeSum(txs, precisionMap = PRECISION_MAP) {
 module.exports = {
   computeSum: computeSum,
   createSumObj: createSumObj,
+  combineCurrencyMaps: combineCurrencyMaps,
 }

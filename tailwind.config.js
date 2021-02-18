c2501315e8f1bdbe5ea91f16e96cfa3d6d9129ba
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2021-02-18 11:53:54 -0700

    Delete old website, add new Arcology Builders static site.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  theme: {
+    // Some useful comment
+    extend: {
+      colors: {
+        //'primary': '#000000',
+        'primary': '#3b7977',
+        'secondary': '#57a99a',
+      },
+      height: {
+        '128': '20rem',
+        'half': '50vh',
+        '3/4': '75vh'
+      },
+    },
+  },
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  variants: {},
+  plugins: [],
+}
